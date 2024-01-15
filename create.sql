7350c0900736511689cc238018f5f2ec40c53c80
Rodrigo Branas <rodrigo.branas@gmail.com>
2024-01-15 17:36:41 -0300

    updated

---
+++ create.sql
@@ -0,0 +1,13 @@
+drop schema cccat15 cascade;
+
+create schema cccat15;
+
+create table cccat15.account (
+	account_id uuid primary key,
+	name text not null,
+	email text not null,
+	cpf text not null,
+	car_plate text null,
+	is_passenger boolean not null default false,
+	is_driver boolean not null default false
+);
