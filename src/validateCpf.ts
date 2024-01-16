7a15ce936b1ecffd776a63d50132aea297bb36e1
Rodrigo Branas <rodrigo.branas@gmail.com>
2024-01-16 12:23:39 -0300

    updated

--- src/validateCpf.ts
+++ src/validateCpf.ts
@@ -1,58 +1,36 @@
-export function validateCpf (str: string) {
-	if (str !== null) {
-		if (str !== undefined) {
-			if (str.length >= 11 && str.length <= 14){
-				// cleaning cpf
-				str=str
-					.replace('.','')
-					.replace('.','')
-					.replace('-','')
-					.replace(" ","");  
-	
-				if (!str.split("").every(c => c === str[0])) {
-					try{  
-						let     d1, d2;  
-						let     dg1, dg2, rest;  
-						let     digito;  
-							let     nDigResult;  
-						d1 = d2 = 0;  
-						dg1 = dg2 = rest = 0;  
-							
-						for (let nCount = 1; nCount < str.length -1; nCount++) {  
-							// if (isNaN(parseInt(str.substring(nCount -1, nCount)))) {
-							// 	return false;
-							// } else {
-	
-								digito = parseInt(str.substring(nCount -1, nCount));  							
-								d1 = d1 + ( 11 - nCount ) * digito;  
-				
-								d2 = d2 + ( 12 - nCount ) * digito;  
-							// }
-						};  
-							
-						rest = (d1 % 11);  
-				
-						dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;  
-						d2 += 2 * dg1;  
-						rest = (d2 % 11);  
-						if (rest < 2)  
-							dg2 = 0;  
-						else  
-							dg2 = 11 - rest;  
-				
-							let nDigVerific = str.substring(str.length-2, str.length);  
-						nDigResult = "" + dg1 + "" + dg2;  
-						return nDigVerific == nDigResult;
-						
-					// just in case...
-					}catch (e){  
-						console.error("Erro !"+e);  
-	
-						return false;  
-					}  
-				} else return false;
-	
-			}else return false;
-		} else return false;
-	} else return false;
+export function validateCpf (rawCpf: string) {
+	if (!rawCpf) return false;
+	const cpf = removeNonDigits(rawCpf);
+	if (isInvalidLength(cpf)) return false;
+	if (hasAllDigitsEqual(cpf)) return false;
+	const digit1 = calculateDigit(cpf, 10);
+	const digit2 = calculateDigit(cpf, 11);
+	return extractDigit(cpf) === `${digit1}${digit2}`;
+}
+
+function removeNonDigits (cpf: string) {
+	return cpf.replace(/\D/g, "");
+}
+
+function isInvalidLength (cpf: string) {
+	const CPF_LENGTH = 11;
+	return cpf.length !== CPF_LENGTH;
+}
+
+function hasAllDigitsEqual (cpf: string) {
+	const [firstCpfDigit] = cpf;
+	return [...cpf].every(digit => digit === firstCpfDigit);
+}
+
+function calculateDigit (cpf: string, factor: number) {
+	let total = 0;
+	for (const digit of cpf) {
+		if (factor > 1) total += parseInt(digit) * factor--;
+	}
+	const rest = total%11;
+	return (rest < 2) ? 0 : 11 - rest;
+}
+
+function extractDigit (cpf: string) {
+	return cpf.slice(9);
 }
---
+++ test/validateCpf.test.ts
@@ -0,0 +1,20 @@
+import { validateCpf } from "../src/validateCpf";
+
+test.each([
+	"97456321558",
+	"71428793860",
+	"87748248800"
+])("Deve testar se o cpf é válido: %s", function (cpf: string) {
+	const isValid = validateCpf(cpf);
+	expect(isValid).toBe(true);
+});
+
+test.each([
+	"8774824880",
+	null,
+	undefined,
+	"11111111111"
+])("Deve testar se o cpf é inválido: %s", function (cpf: any) {
+	const isValid = validateCpf(cpf);
+	expect(isValid).toBe(false);
+});
